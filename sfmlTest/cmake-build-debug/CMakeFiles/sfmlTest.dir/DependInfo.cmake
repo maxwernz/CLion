bcaffe05a1cf2c5193a58c81a5e1c04c819ccee2
Maximilian Wernz <mawernz@outlook.de>
2022-06-29 17:43:39 +0200

    first commit

---
+++ sfmlTest/cmake-build-debug/CMakeFiles/sfmlTest.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/max/development/CLion/sfmlTest/common.cpp" "CMakeFiles/sfmlTest.dir/common.cpp.o" "gcc" "CMakeFiles/sfmlTest.dir/common.cpp.o.d"
+  "/Users/max/development/CLion/sfmlTest/ooptool.cpp" "CMakeFiles/sfmlTest.dir/ooptool.cpp.o" "gcc" "CMakeFiles/sfmlTest.dir/ooptool.cpp.o.d"
+  "/Users/max/development/CLion/sfmlTest/ooptoolg.cpp" "CMakeFiles/sfmlTest.dir/ooptoolg.cpp.o" "gcc" "CMakeFiles/sfmlTest.dir/ooptoolg.cpp.o.d"
+  "/Users/max/development/CLion/sfmlTest/polynom.cpp" "CMakeFiles/sfmlTest.dir/polynom.cpp.o" "gcc" "CMakeFiles/sfmlTest.dir/polynom.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sfmlTest/cmake-build-debug/CMakeFiles/sfmlTest.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfmlTest.dir/common.cpp.o"
+  "CMakeFiles/sfmlTest.dir/common.cpp.o.d"
+  "CMakeFiles/sfmlTest.dir/ooptool.cpp.o"
+  "CMakeFiles/sfmlTest.dir/ooptool.cpp.o.d"
+  "CMakeFiles/sfmlTest.dir/ooptoolg.cpp.o"
+  "CMakeFiles/sfmlTest.dir/ooptoolg.cpp.o.d"
+  "CMakeFiles/sfmlTest.dir/polynom.cpp.o"
+  "CMakeFiles/sfmlTest.dir/polynom.cpp.o.d"
+  "sfmlTest"
+  "sfmlTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfmlTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
